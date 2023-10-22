1564ac6a0c246c3ece0fc9bcb1f97c7850dd12b4
Kayc <ghstkc@gmail.com>
2023-10-22 20:49:41 +0545

    First commit

---
+++ docker-installation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install necessary packages
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Add Docker repository to apt sources
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker engine
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Install Docker Compose
+sudo apt-get update
+sudo apt-get install -y docker-compose
+
+# Print installation status
+echo "Docker and Docker Compose have been installed successfully."
